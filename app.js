f2e49900bad09fcf456348b2f775b4a6b07af405
jaspreet571 <138706287+jaspreet571@users.noreply.github.com>
2023-07-28 16:01:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-function calculate(num1, num2, operator) {
-    switch(operator) {
-    case '+':
-    return num1 + num2;
-    case '-':
-    return num1 - num2;
-    case '*':
-    return num1 * num2;
-    case '/':
-    if(num2 != 0) {
-    return num1 / num2;
+const form = document.getElementById('converter-form');
+const amountInput = document.getElementById('amount');
+const currencySelect = document.getElementById('currency');
+const resultDiv = document.getElementById('result');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const amount = parseFloat(amountInput.value);
+  const currency = currencySelect.value;
+
+  if (isNaN(amount)) {
+    alert('Please enter a valid amount');
+    return;
+  }
+
+  const apiKey = 'D1FF05E5-5DED-4F6B-B595-D451C85AC788'; // Replace with your CoinAPI.io API key
+  const url = `https://rest.coinapi.io/v1/exchangerate/BTC/${currency}?apikey=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (response.ok) {
+      const convertedAmount = amount * data.rate;
+      resultDiv.textContent = `${amount} BTC is equal to ${convertedAmount.toFixed(2)} ${currency}`;
     } else {
-    return 'Error: Division by zero is not allowed.';
+      alert(`Error: ${data.error}`);
     }
-    default:
-    return 'Error: Unknown operator.';
-    }
-    }
+  } catch (error) {
+    alert('Failed to fetch conversion rate');
+  }
+});
